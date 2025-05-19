101a879428371c826f84dc8d12ae30f01bd388c4
Sultan <sultanisabayev2@gmail.com>
2025-05-19 17:49:34 +0500

    added tableView, custom cell

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Sultan Akhmetbek on 19.05.2025.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet var imageViewCell: UIImageView!
+    @IBOutlet var labelViewCell: UILabel!
+    @IBOutlet var buttonCell: UIButton!
+}
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Sultan Akhmetbek on 15.05.2025.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else { return 0 }
+        
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell, with: indexPath)
+        return imageListCell
+    }
+    
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        cell.labelViewCell.text = dateFormatter.string(from: Date())
+        cell.labelViewCell.textColor = .white
+        cell.labelViewCell.font = .systemFont(ofSize: 13, weight: .bold)
+        
+        let blurEffect = UIBlurEffect(style: .systemThinMaterial)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.contentView.addSubview(cell.labelViewCell)
+        cell.contentView.addSubview(blurView)
+        
+        guard let image = UIImage(named: "\(indexPath.row)") else { return }
+        cell.imageViewCell.image = image
+        
+        let buttonImageName = indexPath.row % 2 == 0 ? "Active" : "No Active"
+        cell.buttonCell.setImage(UIImage(named: buttonImageName), for: .normal)
+        cell.buttonCell.setTitle(nil, for: .normal)
+    }
+}
